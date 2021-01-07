521a30b2216196c75d0cfdd2a30b0bd52e9e1926
Zubair Mukhi <zxm132@case.edu>
2021-01-07 17:02:20 -0500

    Create Account page implemented locally

--- casesquirrels/squirrelsite/urls.py
+++ casesquirrels/squirrelsite/urls.py
@@ -7,6 +7,5 @@ app_name= 'squirrels'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     path('secret', views.SecretView.as_view(), name='secret'),
-    path('newuser', views.NewUserView.as_view(), name='newuser'),
-
+    path('newuser', views.signup, name='newuser'),
 ]
--- casesquirrels/squirrelsite/views.py
+++ casesquirrels/squirrelsite/views.py
@@ -1,9 +1,11 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import HttpResponse
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, render, redirect
 from django.views import generic
+from django.contrib.auth import authenticate, login
 
 from .models import Merch
+from .forms import NewUserForm
 
 
 class IndexView(generic.TemplateView):
@@ -12,5 +14,19 @@ class IndexView(generic.TemplateView):
 class SecretView(LoginRequiredMixin, generic.TemplateView):
     template_name=  'squirrelsite/secret.html'
 
-class NewUserView(generic.TemplateView):
-    template_name = 'squirrelsite/newuser.html'
+# class NewUserView(generic.TemplateView):
+#     template_name = 'squirrelsite/newuser.html'
+
+def signup(request):
+    if request.method == 'POST':
+        form = NewUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('squirrels:index')
+    else:
+        form = NewUserForm()
+    return render(request, 'squirrelsite/newuser.html', {'form':form})
